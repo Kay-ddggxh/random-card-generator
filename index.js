d17adea33a6fad54d6bbdd5014c9a5e148da5ed9
Kathrin-ddggxh <74541092+Kathrin-ddggxh@users.noreply.github.com>
2021-12-27 17:02:34 +0000

    made "show image" button work
    function getCardImage() now fully functioning by simply running random numbers (0-51) through an array listing all card png files

--- index.js
+++ index.js
@@ -1,6 +1,59 @@
+let allCards = [
+  "img/cards/2_of_clubs.png",
+  "img/cards/2_of_diamonds.png",
+  "img/cards/2_of_hearts.png",
+  "img/cards/2_of_spades.png",
+  "img/cards/3_of_clubs.png",
+  "img/cards/3_of_diamonds.png",
+  "img/cards/3_of_hearts.png",
+  "img/cards/3_of_spades.png",
+  "img/cards/4_of_clubs.png",
+  "img/cards/4_of_diamonds.png",
+  "img/cards/4_of_hearts.png",
+  "img/cards/4_of_spades.png",
+  "img/cards/5_of_clubs.png",
+  "img/cards/5_of_diamonds.png",
+  "img/cards/5_of_hearts.png",
+  "img/cards/5_of_spades.png",
+  "img/cards/6_of_clubs.png",
+  "img/cards/6_of_diamonds.png",
+  "img/cards/6_of_hearts.png",
+  "img/cards/6_of_spades.png",
+  "img/cards/7_of_clubs.png",
+  "img/cards/7_of_diamonds.png",
+  "img/cards/7_of_hearts.png",
+  "img/cards/7_of_spades.png",
+  "img/cards/8_of_clubs.png",
+  "img/cards/8_of_diamonds.png",
+  "img/cards/8_of_hearts.png",
+  "img/cards/8_of_spades.png",
+  "img/cards/9_of_clubs.png",
+  "img/cards/9_of_diamonds.png",
+  "img/cards/9_of_hearts.png",
+  "img/cards/9_of_spades.png",
+  "img/cards/10_of_clubs.png",
+  "img/cards/10_of_diamonds.png",
+  "img/cards/10_of_hearts.png",
+  "img/cards/10_of_spades.png",
+  "img/cards/ace_of_clubs.png",
+  "img/cards/ace_of_diamonds.png",
+  "img/cards/ace_of_hearts.png",
+  "img/cards/ace_of_spades.png",
+  "img/cards/jack_of_clubs2.png",
+  "img/cards/jack_of_diamonds2.png",
+  "img/cards/jack_of_hearts2.png",
+  "img/cards/jack_of_spades2.png",
+  "img/cards/king_of_clubs2.png",
+  "img/cards/king_of_diamonds2.png",
+  "img/cards/king_of_hearts2.png",
+  "img/cards/king_of_spades2.png",
+  "img/cards/queen_of_clubs2.png",
+  "img/cards/queen_of_diamonds2.png",
+  "img/cards/queen_of_hearts2.png",
+  "img/cards/queen_of_spades2.png",
+];
 let cardEl = document.getElementById("card-el");
 let suitEl = document.getElementById("suit-el");
-let cardImg = document.getElementById("card-img");
 
 function drawCard() {
   generateCard();
@@ -51,67 +104,9 @@ function generateSuit() {
   return suit; //using return here so I can use entire function as parameter
 }
 
-function getCardImage(card, suit) {
-  let allCards = [
-    "img/cards/2_of_clubs.png",
-    "img/cards/2_of_diamonds.png",
-    "img/cards/2_of_hearts.png",
-    "img/cards/2_of_spades.png",
-    "img/cards/3_of_clubs.png",
-    "img/cards/3_of_diamonds.png",
-    "img/cards/3_of_hearts.png",
-    "img/cards/3_of_spades.png",
-    "img/cards/4_of_clubs.png",
-    "img/cards/4_of_diamonds.png",
-    "img/cards/4_of_hearts.png",
-    "img/cards/4_of_spades.png",
-    "img/cards/5_of_clubs.png",
-    "img/cards/5_of_diamonds.png",
-    "img/cards/5_of_hearts.png",
-    "img/cards/5_of_spades.png",
-    "img/cards/6_of_clubs.png",
-    "img/cards/6_of_diamonds.png",
-    "img/cards/6_of_hearts.png",
-    "img/cards/6_of_spades.png",
-    "img/cards/7_of_clubs.png",
-    "img/cards/7_of_diamonds.png",
-    "img/cards/7_of_hearts.png",
-    "img/cards/7_of_spades.png",
-    "img/cards/8_of_clubs.png",
-    "img/cards/8_of_diamonds.png",
-    "img/cards/8_of_hearts.png",
-    "img/cards/8_of_spades.png",
-    "img/cards/9_of_clubs.png",
-    "img/cards/9_of_diamonds.png",
-    "img/cards/9_of_hearts.png",
-    "img/cards/9_of_spades.png",
-    "img/cards/10_of_clubs.png",
-    "img/cards/10_of_diamonds.png",
-    "img/cards/10_of_hearts.png",
-    "img/cards/10_of_spades.png",
-    "img/cards/ace_of_clubs.png",
-    "img/cards/ace_of_diamonds.png",
-    "img/cards/ace_of_hearts.png",
-    "img/cards/ace_of_spades.png",
-    "img/cards/jack_of_clubs2.png",
-    "img/cards/jack_of_diamonds2.png",
-    "img/cards/jack_of_hearts2.png",
-    "img/cards/jack_of_spades2.png",
-    "img/cards/king_of_clubs2.png",
-    "img/cards/king_of_diamonds2.png",
-    "img/cards/king_of_hearts2.png",
-    "img/cards/king_of_spades2.png",
-    "img/cards/queen_of_clubs2.png",
-    "img/cards/queen_of_diamonds2.png",
-    "img/cards/queen_of_hearts2.png",
-    "img/cards/queen_of_spades2.png",
-  ];
-  if (card === 2 && suit === 4) {
-    cardImg = "img/cards/2_of_clubs.png";
-  }
+function getCardImage() {
+  let randomCardImg = allCards[Math.floor(Math.random() * 52)];
 
-  document.getElementById("card-img").src = cardImg;
+  document.getElementById("card-img").src = randomCardImg;
   document.getElementById("card-img").style.display = "block";
 }
-
-getCardImage(generateCard(), generateSuit());
