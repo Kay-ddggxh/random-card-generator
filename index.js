47a7aa7c28a22bc36e99164dac1d2e9c27e0c4ca
Kathrin-ddggxh <74541092+Kathrin-ddggxh@users.noreply.github.com>
2021-12-27 09:11:40 +0000

    created array for all card images

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function generateCard() {
     randomCard = "KING";
   }
   cardEl.innerHTML = randomCard;
+
+  return randomCard;
 }
 
 function generateSuit() {
@@ -45,10 +47,71 @@ function generateSuit() {
   } else {
     suitEl.style.color = "black";
   }
+
+  return suit; //using return here so I can use entire function as parameter
 }
 
-function getCardImage() {
-  cardImg = "img/cards/2_of_clubs.png";
+function getCardImage(card, suit) {
+  let allCards = [
+    "img/cards/2_of_clubs.png",
+    "img/cards/2_of_diamonds.png",
+    "img/cards/2_of_hearts.png",
+    "img/cards/2_of_spades.png",
+    "img/cards/3_of_clubs.png",
+    "img/cards/3_of_diamonds.png",
+    "img/cards/3_of_hearts.png",
+    "img/cards/3_of_spades.png",
+    "img/cards/4_of_clubs.png",
+    "img/cards/4_of_diamonds.png",
+    "img/cards/4_of_hearts.png",
+    "img/cards/4_of_spades.png",
+    "img/cards/5_of_clubs.png",
+    "img/cards/5_of_diamonds.png",
+    "img/cards/5_of_hearts.png",
+    "img/cards/5_of_spades.png",
+    "img/cards/6_of_clubs.png",
+    "img/cards/6_of_diamonds.png",
+    "img/cards/6_of_hearts.png",
+    "img/cards/6_of_spades.png",
+    "img/cards/7_of_clubs.png",
+    "img/cards/7_of_diamonds.png",
+    "img/cards/7_of_hearts.png",
+    "img/cards/7_of_spades.png",
+    "img/cards/8_of_clubs.png",
+    "img/cards/8_of_diamonds.png",
+    "img/cards/8_of_hearts.png",
+    "img/cards/8_of_spades.png",
+    "img/cards/9_of_clubs.png",
+    "img/cards/9_of_diamonds.png",
+    "img/cards/9_of_hearts.png",
+    "img/cards/9_of_spades.png",
+    "img/cards/10_of_clubs.png",
+    "img/cards/10_of_diamonds.png",
+    "img/cards/10_of_hearts.png",
+    "img/cards/10_of_spades.png",
+    "img/cards/ace_of_clubs.png",
+    "img/cards/ace_of_diamonds.png",
+    "img/cards/ace_of_hearts.png",
+    "img/cards/ace_of_spades.png",
+    "img/cards/jack_of_clubs2.png",
+    "img/cards/jack_of_diamonds2.png",
+    "img/cards/jack_of_hearts2.png",
+    "img/cards/jack_of_spades2.png",
+    "img/cards/king_of_clubs2.png",
+    "img/cards/king_of_diamonds2.png",
+    "img/cards/king_of_hearts2.png",
+    "img/cards/king_of_spades2.png",
+    "img/cards/queen_of_clubs2.png",
+    "img/cards/queen_of_diamonds2.png",
+    "img/cards/queen_of_hearts2.png",
+    "img/cards/queen_of_spades2.png",
+  ];
+  if (card === 2 && suit === 4) {
+    cardImg = "img/cards/2_of_clubs.png";
+  }
+
   document.getElementById("card-img").src = cardImg;
   document.getElementById("card-img").style.display = "block";
 }
+
+getCardImage(generateCard(), generateSuit());
